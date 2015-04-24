023b5534b1aad5c896279b53b982562f85d5c967
fywxin <6697275@163.com>
2015-04-24 11:20:52 +0800

    用户登入绑定勾事件 增加排序接口

--- whale/system-parent/system-jdbc/src/main/java/org/whale/system/base/UserContextAccessor.java
+++ whale/system-parent/system-jdbc/src/main/java/org/whale/system/base/UserContextAccessor.java
@@ -2,7 +2,9 @@ package org.whale.system.base;
 
 import org.springframework.core.PriorityOrdered;
 
-
+/**
+ * 
+ */
 public interface UserContextAccessor extends PriorityOrdered{
 
 	void addCustomDatas(UserContext uc);
