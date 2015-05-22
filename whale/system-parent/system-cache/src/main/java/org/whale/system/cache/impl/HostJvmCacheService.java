a8e45119ef173602d8c65b4b7b1bab7b5bbafdc3
fywxin <6697275@163.com>
2015-05-22 13:10:18 +0800

    [improve]增加组合缓存与最热JVM缓存

---
+++ whale/system-parent/system-cache/src/main/java/org/whale/system/cache/impl/HostJvmCacheService.java
@@ -0,0 +1,71 @@
+package org.whale.system.cache.impl;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 缓存最新最热的数据
+ * 
+ * @author 王金绍
+ * @date 2015年5月22日 下午1:09:16
+ */
+public class HostJvmCacheService<M extends Serializable> extends AbstractCacheService<M> {
+
+	@Override
+	public void clear(String cacheName) {
+		
+		
+	}
+
+	@Override
+	public Object getNativeCache() {
+		
+		return null;
+	}
+
+	@Override
+	public Set<String> getKeys(String cacheName) {
+		
+		return null;
+	}
+
+	@Override
+	public void doPut(String cacheName, String key, M value, Integer seconds) {
+		
+		
+	}
+
+	@Override
+	public void mdoPut(String cacheName, Map<String, M> keyValues,
+			Integer seconds) {
+		
+		
+	}
+
+	@Override
+	public M doGet(String cacheName, String key) {
+		
+		return null;
+	}
+
+	@Override
+	public List<M> mdoGet(String cacheName, List<String> keys) {
+		
+		return null;
+	}
+
+	@Override
+	public void doDel(String cacheName, String key) {
+		
+		
+	}
+
+	@Override
+	public void mdoDel(String cacheName, List<String> keys) {
+		
+		
+	}
+
+}
