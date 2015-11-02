4a75a6399aa5a46adf0d009efc762c7adb0ced37
fywxin <6697275@163.com>
2015-11-02 14:30:40 +0800

    [code] 验证码异常日志打印

--- whale/system-parent/system-web/src/main/java/org/whale/system/servlet/VerifiedServlet.java
+++ whale/system-parent/system-web/src/main/java/org/whale/system/servlet/VerifiedServlet.java
@@ -9,10 +9,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.whale.system.common.constant.SysConstant;
 
 
 public class VerifiedServlet extends HttpServlet {
+	
+	private static final Logger logger = LoggerFactory.getLogger(VerifiedServlet.class);
 
 	private static final long serialVersionUID = -34545L;
 
@@ -43,6 +47,8 @@ public class VerifiedServlet extends HttpServlet {
 			codeGenerator.setImgHeight(22);
 			codeGenerator.output(out, "jpg", codeGenerator.createImage(strb.toString()));
 		} catch (Exception e) {
+			logger.error("校验码生产异常：", e);
+			//TODO 自动创建temp目录
 		}finally{
 			if(out != null)
 				out.close();
