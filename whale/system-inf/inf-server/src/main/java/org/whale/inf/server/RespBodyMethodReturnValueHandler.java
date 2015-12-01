984637ac3f551b2d84b2422a184c988b9b3326e5
fywxin <fywxin@6697275@163.com>
2015-12-01 23:41:39 +0800

    [code] 接口修改

--- whale/system-inf/inf-server/src/main/java/org/whale/inf/server/RespBodyMethodReturnValueHandler.java
+++ whale/system-inf/inf-server/src/main/java/org/whale/inf/server/RespBodyMethodReturnValueHandler.java
@@ -2,12 +2,16 @@ package org.whale.inf.server;
 
 import java.lang.annotation.Annotation;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import org.whale.inf.common.EncryptService;
+import org.whale.inf.common.SignService;
 import org.whale.system.annotation.web.RespBody;
 import org.whale.system.common.util.ThreadContext;
 
@@ -16,9 +20,11 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 
 public class RespBodyMethodReturnValueHandler implements HandlerMethodReturnValueHandler {
 	
-	public RespBodyMethodReturnValueHandler(){
-		System.out.println("init-------------");
-	}
+	@Autowired
+	private EncryptService encryptService;
+	
+	@Autowired
+	private SignService signService;
 
 	@Override
 	public boolean supportsReturnType(MethodParameter returnType) {
@@ -36,20 +42,26 @@ public class RespBodyMethodReturnValueHandler implements HandlerMethodReturnValu
 
 	@Override
 	public void handleReturnValue(Object returnValue, MethodParameter returnType, ModelAndViewContainer mavContainer, NativeWebRequest webRequest) throws Exception {
+		//设置true，防止变为页面跳转处理
+		mavContainer.setRequestHandled(true);
 		ServerContext context = (ServerContext)ThreadContext.getContext().get(ServerContext.THREAD_KEY);
+		ServletOutputStream out = null;
 		try{
 			HttpServletResponse response = webRequest.getNativeResponse(HttpServletResponse.class);
+			out = response.getOutputStream();
 			
 			//String text = JSON.toJSONString(obj, this.getFeatures());
 	        //保证客户端可以反序列化成功
 	        String text = JSON.toJSONString(returnValue, SerializerFeature.WriteClassName);
 	        
 	        byte[] bytes = text.getBytes("UTF-8");
-	        
-	        
-	        response.getOutputStream().write(bytes);
+	        out.write(bytes);
+	        out.flush();
 		}finally{
 			ThreadContext.getContext().remove(ServerContext.THREAD_KEY);
+			if(out != null){
+				out.close();
+			}
 		}
 	}
 
