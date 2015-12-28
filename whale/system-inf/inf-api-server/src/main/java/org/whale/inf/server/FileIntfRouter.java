0e6f4af87da4e29719c8634dae18f8368ec310b6
fywxin <6697275@163.com>
2015-12-29 00:02:35 +0800

    [code] 文件上传

--- whale/system-inf/inf-api-server/src/main/java/org/whale/inf/server/FileIntfRouter.java
+++ whale/system-inf/inf-api-server/src/main/java/org/whale/inf/server/FileIntfRouter.java
@@ -1,21 +1,37 @@
 package org.whale.inf.server;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.whale.inf.api.FileBean;
-import org.whale.inf.api.FileIntf;
 import org.whale.inf.common.Result;
+import org.whale.system.annotation.web.RespBody;
 
-public class FileIntfRouter implements FileIntf {
+@Controller
+@RequestMapping("/fileIntf")
+public class FileIntfRouter {
 
-	@Override
-	public Result<String> uploadFileBean(FileBean fileBean) {
-		// TODO Auto-generated method stub
-		return null;
+	@RespBody
+	@RequestMapping("/uploadFileBean")
+	public Result<String> uploadFileBean(FileBean fileBean) throws IOException {
+		File file = new File("c://1.jpg");
+		FileOutputStream fos = new FileOutputStream(file);
+		IOUtils.write(fileBean.getBytes(), fos);
+		
+		return Result.success(fileBean.getFileName());
 	}
 
-	@Override
-	public Result<String> uploadFile(byte[] datas) {
-		// TODO Auto-generated method stub
-		return null;
+	@RespBody
+	@RequestMapping("/uploadFile")
+	public Result<String> uploadFile(byte[] datas) throws IOException {
+		File file = new File("c://2.jpg");
+		FileOutputStream fos = new FileOutputStream(file);
+		IOUtils.write(datas, fos);
+		return Result.success(file.getName());
 	}
 
 }
--- whale/system-inf/inf-api-server/src/main/java/org/whale/inf/server/FiltRouter.java
+++ whale/system-inf/inf-api-server/src/main/java/org/whale/inf/server/FiltRouter.java
@@ -1,39 +1,24 @@
-package org.whale.inf.client;
+package org.whale.inf.server;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.whale.inf.api.FileBean;
-import org.whale.inf.api.FileIntf;
-import org.whale.inf.api.UserInf;
 import org.whale.system.base.Rs;
-import org.whale.inf.api.User;
 
 @Controller
-@RequestMapping("/user")
-public class ClientRouter {
+@RequestMapping("/file")
+public class FiltRouter {
 
-	@Autowired
-	private UserInf userInf;
-	
-	@Autowired
-	private FileIntf fileIntf;
-	
-	@ResponseBody
-	@RequestMapping("/get")
-	public Rs get(Long id){
-		User user = this.userInf.get(id);
-		return Rs.success(user);
-	}
-	
 	@ResponseBody
 	@RequestMapping("/upload")
 	public Rs upload(HttpServletRequest request){
@@ -50,12 +35,12 @@ public class ClientRouter {
 			try {
 				multipartFile = entry.getValue();
 				
-				FileBean fileBean = new FileBean();
-				fileBean.setBytes(multipartFile.getBytes());
-				fileBean.setFileName(multipartFile.getName());
-				
-				String name = this.fileIntf.uploadFileBean(fileBean);
-				return Rs.success(name);
+				File file = new File("c://"+entry.getKey());
+				FileOutputStream fos = new FileOutputStream(file);
+				IOUtils.write(multipartFile.getBytes(), fos);
+				fos.flush();
+				fos.close();
+				return Rs.success(file.getName());
 			} catch (Exception e) {
 				return Rs.fail("保存文件异常-"+e.getMessage());
 			}
